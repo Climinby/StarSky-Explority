36da00d2cb74c034aad4269e520019f6bc1aa2fe
Climinby <climinby@gmail.com>
2024-10-18 23:56:43 +0800

    v0.0.2c

--- src/client/java/com/climinby/starsky_e/client/entity/model/TsukiNoTamiEntityModel.java
+++ src/client/java/com/climinby/starsky_e/client/entity/model/TsukiNoTamiEntityModel.java
@@ -4,30 +4,36 @@ import com.climinby.starsky_e.entity.TsukiNoTamiEntity;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.EntityModel;
-import net.minecraft.client.render.entity.model.EntityModelPartNames;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.MathHelper;
+
+import java.util.List;
 
 public class TsukiNoTamiEntityModel extends EntityModel<TsukiNoTamiEntity> {
-    private final ModelPart legs;
+    private final ModelPart main;
+    private final ModelPart right_hand;
+    private final ModelPart left_hand;
     private final ModelPart head;
     private final ModelPart hat;
     private final ModelPart ears;
-    private final ModelPart left_hand;
-    private final ModelPart right_hand;
-    private final ModelPart bb_main;
+    private final ModelPart ohead;
+    private final ModelPart body;
+    private final ModelPart left_leg;
+    private final ModelPart right_leg;
 
     public TsukiNoTamiEntityModel() {
         this(getTexturedModelData().createModel());
     }
     public TsukiNoTamiEntityModel(ModelPart root) {
-        this.legs = root.getChild("legs");
-        this.head = root.getChild("head");
-        this.hat = head.getChild("hat");
-        this.ears = head.getChild("ears");
-        this.left_hand = root.getChild("left_hand");
-        this.right_hand = root.getChild("right_hand");
-        this.bb_main = root.getChild("bb_main");
+        this.main = root.getChild("main");
+        this.right_hand = this.main.getChild("right_hand");
+        this.left_hand = this.main.getChild("left_hand");
+        this.head = this.main.getChild("head");
+        this.hat = this.head.getChild("hat");
+        this.ears = this.head.getChild("ears");
+        this.ohead = this.head.getChild("ohead");
+        this.body = this.main.getChild("body");
+        this.left_leg = this.main.getChild("left_leg");
+        this.right_leg = this.main.getChild("right_leg");
     }
 
     @Override
@@ -38,57 +44,74 @@ public class TsukiNoTamiEntityModel extends EntityModel<TsukiNoTamiEntity> {
             this.head.pitch = headPitch;
         } else {
             this.left_hand.pitch = 0F;
-            this.left_hand.pivotY = 24F;
+            this.left_hand.pivotY = 0F;
             this.left_hand.pivotZ = 0F;
             this.right_hand.pitch = 0F;
-            this.right_hand.pivotY = 24F;
+            this.right_hand.pivotY = 0F;
             this.right_hand.pivotZ = 0F;
+
+            this.right_leg.pitch = 0F;
+            this.right_leg.pivotY = 0F;
+            this.right_leg.pivotZ = 0F;
+            this.left_leg.pitch = 0F;
+            this.left_leg.pivotY = -6.0F;
+            this.left_leg.pivotZ = 0F;
             this.head.yaw = headYaw;
             this.head.pitch = headPitch;
         }
     }
 
-    private void playHurtAnimation(float animationProgress) {
-        this.left_hand.pitch = animationProgress;
-        this.left_hand.pivotY = 24.0F * ((float) Math.cos(animationProgress) - 1.0F) + 24.0F;
-        this.left_hand.pivotZ = 24.0F * (float) Math.sin(animationProgress);
-        this.right_hand.pitch = -animationProgress;
-        this.right_hand.pivotY = 24.0F * ((float) Math.cos(animationProgress) - 1.0F) + 24.0F;
-        this.right_hand.pivotZ = - (24.0F * (float) Math.sin(animationProgress));
+    /**
+     * @param rotationAngle a number to be in radians
+     */
+    private void playHurtAnimation(float rotationAngle) {
+        this.left_hand.pitch = rotationAngle;
+        this.left_hand.pivotY = 23.0F * ((float) Math.cos(rotationAngle) - 1.0F);
+        this.left_hand.pivotZ = 23.0F * (float) Math.sin(rotationAngle);
+        this.right_hand.pitch = -rotationAngle;
+        this.right_hand.pivotY = 23.0F * ((float) Math.cos(rotationAngle) - 1.0F);
+        this.right_hand.pivotZ = - (23.0F * (float) Math.sin(rotationAngle));
+        this.left_leg.pitch = -rotationAngle;
+        this.left_leg.pivotY = 6.0F * ((float) Math.cos(rotationAngle) - 1.0F) - 6.0F;
+        this.left_leg.pivotZ = - (6.0F * (float) Math.sin(rotationAngle));
+        this.right_leg.pitch = rotationAngle;
+        this.right_leg.pivotY = 12.0F * ((float) Math.cos(rotationAngle) - 1.0F);
+        this.right_leg.pivotZ = 12.0F * (float) Math.sin(rotationAngle);
     }
 
     @Override
     public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
-        legs.render(matrices, vertices, light, overlay, red, green, blue, alpha);
-        head.render(matrices, vertices, light, overlay, red, green, blue, alpha);
-        left_hand.render(matrices, vertices, light, overlay, red, green, blue, alpha);
-        right_hand.render(matrices, vertices, light, overlay, red, green, blue, alpha);
-        bb_main.render(matrices, vertices, light, overlay, red, green, blue, alpha);
+        main.render(matrices, vertices, light, overlay, red, green, blue, alpha);
     }
 
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
-        ModelPartData legs = modelPartData.addChild("legs", ModelPartBuilder.create().uv(0, 29).cuboid(2.0F, 11.0F, -5.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F))
-                .uv(28, 0).cuboid(-2.0F, 11.0F, -5.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(-2.0F, 1.0F, 3.0F));
+        ModelPartData main = modelPartData.addChild("main", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 
-        ModelPartData head = modelPartData.addChild("head", ModelPartBuilder.create().uv(0, 15).cuboid(-2.0F, -9.0F, -7.0F, 8.0F, 8.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(-2.0F, 1.0F, 3.0F));
+        ModelPartData right_hand = main.addChild("right_hand", ModelPartBuilder.create().uv(12, 41).cuboid(-7.0F, -24.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-        ModelPartData hat = head.addChild("hat", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData left_hand = main.addChild("left_hand", ModelPartBuilder.create().uv(26, 41).cuboid(4.0F, -24.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+        ModelPartData head = main.addChild("head", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+        ModelPartData hat = head.addChild("hat", ModelPartBuilder.create(), ModelTransform.pivot(-2.0F, -23.0F, 3.0F));
 
         ModelPartData hat_r1 = hat.addChild("hat_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-3.0F, -10.0F, -4.0F, 9.0F, 10.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(0.5F, 1.0F, -1.0F, -0.1745F, 0.0F, 0.0F));
 
-        ModelPartData ears = head.addChild("ears", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData ears = head.addChild("ears", ModelPartBuilder.create(), ModelTransform.pivot(-2.0F, -23.0F, 3.0F));
 
         ModelPartData right_ear_r1 = ears.addChild("right_ear_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-1.0F, -5.0F, 1.0F, 2.0F, 5.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-0.5F, -8.0F, -6.0F, 0.0F, 0.0F, -0.1745F));
 
         ModelPartData left_ear_r1 = ears.addChild("left_ear_r1", ModelPartBuilder.create().uv(0, 15).cuboid(-1.0F, -5.0F, 1.0F, 2.0F, 5.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(4.5F, -8.0F, -6.0F, 0.0F, 0.0F, 0.1745F));
 
-        ModelPartData left_hand = modelPartData.addChild("left_hand", ModelPartBuilder.create().uv(26, 41).cuboid(4.0F, -24.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+        ModelPartData ohead = head.addChild("ohead", ModelPartBuilder.create().uv(0, 15).cuboid(-2.0F, -9.0F, -7.0F, 8.0F, 8.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(-2.0F, -23.0F, 3.0F));
 
-        ModelPartData right_hand = modelPartData.addChild("right_hand", ModelPartBuilder.create().uv(12, 41).cuboid(-7.0F, -24.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+        ModelPartData body = main.addChild("body", ModelPartBuilder.create().uv(24, 25).cuboid(-4.0F, -6.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -18.0F, 0.0F));
 
-        ModelPartData bb_main = modelPartData.addChild("bb_main", ModelPartBuilder.create().uv(24, 25).cuboid(-4.0F, -24.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+        ModelPartData left_leg = main.addChild("left_leg", ModelPartBuilder.create().uv(0, 29).cuboid(-2.0F, -6.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(2.0F, -6.0F, 0.0F));
+
+        ModelPartData right_leg = main.addChild("right_leg", ModelPartBuilder.create().uv(28, 0).cuboid(-4.0F, -12.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
         return TexturedModelData.of(modelData, 64, 64);
     }
 }

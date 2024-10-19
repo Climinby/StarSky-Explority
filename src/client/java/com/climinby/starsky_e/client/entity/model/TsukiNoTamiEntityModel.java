f52e03fd9d583b8bac8f808bffda5153fa0d6c39
Climinby <climinby@gmail.com>
2024-10-19 20:57:22 +0800

    v0.0.2d

--- src/client/java/com/climinby/starsky_e/client/entity/model/TsukiNoTamiEntityModel.java
+++ src/client/java/com/climinby/starsky_e/client/entity/model/TsukiNoTamiEntityModel.java
@@ -5,6 +5,8 @@ import net.minecraft.client.model.*;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
 
 import java.util.List;
 
@@ -38,27 +40,12 @@ public class TsukiNoTamiEntityModel extends EntityModel<TsukiNoTamiEntity> {
 
     @Override
     public void setAngles(TsukiNoTamiEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
-        if (entity.hurtTime > 0) {
-            playHurtAnimation((float) (entity.hurtTime) / 10.0F * (float) (Math.PI / 3.0));
-            this.head.yaw = headYaw;
-            this.head.pitch = headPitch;
-        } else {
-            this.left_hand.pitch = 0F;
-            this.left_hand.pivotY = 0F;
-            this.left_hand.pivotZ = 0F;
-            this.right_hand.pitch = 0F;
-            this.right_hand.pivotY = 0F;
-            this.right_hand.pivotZ = 0F;
-
-            this.right_leg.pitch = 0F;
-            this.right_leg.pivotY = 0F;
-            this.right_leg.pivotZ = 0F;
-            this.left_leg.pitch = 0F;
-            this.left_leg.pivotY = -6.0F;
-            this.left_leg.pivotZ = 0F;
-            this.head.yaw = headYaw;
-            this.head.pitch = headPitch;
-        }
+        this.left_hand.pitch = MathHelper.cos(limbAngle * 0.6662F) * 1.4F * limbDistance;
+        this.right_hand.pitch = MathHelper.cos(limbAngle * 0.6662F + (float)Math.PI) * 1.4F * limbDistance;
+        this.right_leg.pitch = MathHelper.cos(limbAngle * 0.6662F) * 1.4F * limbDistance;
+        this.left_leg.pitch = MathHelper.cos(limbAngle * 0.6662F + (float)Math.PI) * 1.4F * limbDistance;
+        this.head.yaw = headYaw * ((float)Math.PI / 180F);
+        this.head.pitch = headPitch * ((float)Math.PI / 180F);
     }
 
     /**
@@ -66,17 +53,9 @@ public class TsukiNoTamiEntityModel extends EntityModel<TsukiNoTamiEntity> {
      */
     private void playHurtAnimation(float rotationAngle) {
         this.left_hand.pitch = rotationAngle;
-        this.left_hand.pivotY = 23.0F * ((float) Math.cos(rotationAngle) - 1.0F);
-        this.left_hand.pivotZ = 23.0F * (float) Math.sin(rotationAngle);
         this.right_hand.pitch = -rotationAngle;
-        this.right_hand.pivotY = 23.0F * ((float) Math.cos(rotationAngle) - 1.0F);
-        this.right_hand.pivotZ = - (23.0F * (float) Math.sin(rotationAngle));
         this.left_leg.pitch = -rotationAngle;
-        this.left_leg.pivotY = 6.0F * ((float) Math.cos(rotationAngle) - 1.0F) - 6.0F;
-        this.left_leg.pivotZ = - (6.0F * (float) Math.sin(rotationAngle));
         this.right_leg.pitch = rotationAngle;
-        this.right_leg.pivotY = 12.0F * ((float) Math.cos(rotationAngle) - 1.0F);
-        this.right_leg.pivotZ = 12.0F * (float) Math.sin(rotationAngle);
     }
 
     @Override
@@ -89,29 +68,29 @@ public class TsukiNoTamiEntityModel extends EntityModel<TsukiNoTamiEntity> {
         ModelPartData modelPartData = modelData.getRoot();
         ModelPartData main = modelPartData.addChild("main", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 
-        ModelPartData right_hand = main.addChild("right_hand", ModelPartBuilder.create().uv(12, 41).cuboid(-7.0F, -24.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData right_hand = main.addChild("right_hand", ModelPartBuilder.create().uv(12, 41).cuboid(-1.5F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(-5.5F, -22.0F, 0.0F));
 
-        ModelPartData left_hand = main.addChild("left_hand", ModelPartBuilder.create().uv(26, 41).cuboid(4.0F, -24.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData left_hand = main.addChild("left_hand", ModelPartBuilder.create().uv(26, 41).cuboid(-1.5F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(5.5F, -22.0F, 0.0F));
 
-        ModelPartData head = main.addChild("head", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData head = main.addChild("head", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, -24.0F, 1.0F));
 
-        ModelPartData hat = head.addChild("hat", ModelPartBuilder.create(), ModelTransform.pivot(-2.0F, -23.0F, 3.0F));
+        ModelPartData hat = head.addChild("hat", ModelPartBuilder.create(), ModelTransform.pivot(-2.0F, 1.0F, 3.0F));
 
         ModelPartData hat_r1 = hat.addChild("hat_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-3.0F, -10.0F, -4.0F, 9.0F, 10.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(0.5F, 1.0F, -1.0F, -0.1745F, 0.0F, 0.0F));
 
-        ModelPartData ears = head.addChild("ears", ModelPartBuilder.create(), ModelTransform.pivot(-2.0F, -23.0F, 3.0F));
+        ModelPartData ears = head.addChild("ears", ModelPartBuilder.create(), ModelTransform.pivot(-2.0F, 1.0F, 3.0F));
 
-        ModelPartData right_ear_r1 = ears.addChild("right_ear_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-1.0F, -5.0F, 1.0F, 2.0F, 5.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-0.5F, -8.0F, -6.0F, 0.0F, 0.0F, -0.1745F));
+        ModelPartData right_ear_r1 = ears.addChild("right_ear_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-1.0F, -5.0F, 0.0F, 2.0F, 5.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-0.5F, -8.0F, -5.0F, 0.0F, 0.0F, -0.1745F));
 
-        ModelPartData left_ear_r1 = ears.addChild("left_ear_r1", ModelPartBuilder.create().uv(0, 15).cuboid(-1.0F, -5.0F, 1.0F, 2.0F, 5.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(4.5F, -8.0F, -6.0F, 0.0F, 0.0F, 0.1745F));
+        ModelPartData left_ear_r1 = ears.addChild("left_ear_r1", ModelPartBuilder.create().uv(0, 15).cuboid(-1.0F, -5.0F, 0.0F, 2.0F, 5.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(4.5F, -8.0F, -5.0F, 0.0F, 0.0F, 0.1745F));
 
-        ModelPartData ohead = head.addChild("ohead", ModelPartBuilder.create().uv(0, 15).cuboid(-2.0F, -9.0F, -7.0F, 8.0F, 8.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(-2.0F, -23.0F, 3.0F));
+        ModelPartData ohead = head.addChild("ohead", ModelPartBuilder.create().uv(0, 15).cuboid(-2.0F, -9.0F, -7.0F, 8.0F, 8.0F, 6.0F, new Dilation(0.0F)), ModelTransform.pivot(-2.0F, 1.0F, 3.0F));
 
         ModelPartData body = main.addChild("body", ModelPartBuilder.create().uv(24, 25).cuboid(-4.0F, -6.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -18.0F, 0.0F));
 
-        ModelPartData left_leg = main.addChild("left_leg", ModelPartBuilder.create().uv(0, 29).cuboid(-2.0F, -6.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(2.0F, -6.0F, 0.0F));
+        ModelPartData left_leg = main.addChild("left_leg", ModelPartBuilder.create().uv(0, 29).cuboid(-2.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(2.0F, -10.0F, 0.0F));
 
-        ModelPartData right_leg = main.addChild("right_leg", ModelPartBuilder.create().uv(28, 0).cuboid(-4.0F, -12.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData right_leg = main.addChild("right_leg", ModelPartBuilder.create().uv(28, 0).cuboid(-2.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(-2.0F, -10.0F, 0.0F));
         return TexturedModelData.of(modelData, 64, 64);
     }
 }
--- src/main/java/com/climinby/starsky_e/entity/TsukiNoTamiEntity.java
+++ src/main/java/com/climinby/starsky_e/entity/TsukiNoTamiEntity.java
@@ -1,31 +1,97 @@
 package com.climinby.starsky_e.entity;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.mob.ZombifiedPiglinEntity;
+import net.minecraft.entity.passive.RabbitEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class TsukiNoTamiEntity extends PathAwareEntity {
+import java.util.UUID;
+
+public class TsukiNoTamiEntity extends PathAwareEntity implements Angerable {
+    private int angerTime = 0;
+    private UUID targetUuid = null;
+
     protected TsukiNoTamiEntity(EntityType<? extends TsukiNoTamiEntity> entityType, World world) {
         super(entityType, world);
     }
 
     @Override
-    public void handleStatus(byte status) {
-        if(status == 2) {
-            this.hurtTime = 10;
-        } else {
-            super.handleStatus(status);
+    public void tick() {
+        super.tick();
+        if(this.hasAngerTime()) {
+            angerTime--;
         }
+    }
+
+    @Override
+    public void handleStatus(byte status) {
+        super.handleStatus(status);
     }
 
     @Override
     public boolean damage(DamageSource source, float amount) {
         if(super.damage(source, amount)) {
-            this.getWorld().sendEntityStatus(this, (byte) 2);
+            Entity attackerEntity = source.getAttacker();
+            if(attackerEntity != null) {
+                if(attackerEntity instanceof PlayerEntity attackingPlayer) {
+                    this.setAttacking(attackingPlayer);
+                    this.setAttacker(attackingPlayer);
+                } else if(attackerEntity instanceof LivingEntity attacker) {
+                    this.setAttacker(attacker);
+                }
+            }
+            chooseRandomAngerTime();
             return true;
         }
         return false;
+    }
+
+    @Override
+    protected void initGoals() {
+        super.initGoals();
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1, new WanderAroundFarGoal(this, 1.0));
+        this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 16.0F));
+
+        this.targetSelector.add(1, new RevengeGoal(this, TsukiNoTamiEntity.class, RabbitEntity.class));
+        this.targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, 10, true, false, this::isPlayerAggressor));
+        this.targetSelector.add(3, new UniversalAngerGoal<>(this, false));
+    }
+
+    private boolean isPlayerAggressor(LivingEntity entity) {
+        return true;
+    }
+
+    @Override
+    public int getAngerTime() {
+        return angerTime;
+    }
+
+    @Override
+    public void setAngerTime(int angerTime) {
+        this.angerTime = angerTime;
+    }
+
+    @Override
+    public @Nullable UUID getAngryAt() {
+        return targetUuid;
+    }
+
+    @Override
+    public void setAngryAt(@Nullable UUID angryAt) {
+        this.targetUuid = angryAt;
+    }
+
+    @Override
+    public void chooseRandomAngerTime() {
+        this.angerTime = this.random.nextInt(30) + 150;
     }
 }

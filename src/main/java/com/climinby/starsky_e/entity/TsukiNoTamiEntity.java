ec58f958f9e546a088bd5bbdbfa764150bae848d
Climinby <climinby@gmail.com>
2024-10-24 23:20:51 +0800

    v0.0.2e

--- src/main/java/com/climinby/starsky_e/entity/TsukiNoTamiEntity.java
+++ src/main/java/com/climinby/starsky_e/entity/TsukiNoTamiEntity.java
@@ -4,20 +4,14 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.mob.PathAwareEntity;
-import net.minecraft.entity.mob.ZombifiedPiglinEntity;
-import net.minecraft.entity.passive.RabbitEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.UUID;
-
-public class TsukiNoTamiEntity extends PathAwareEntity implements Angerable {
-    private int angerTime = 0;
-    private UUID targetUuid = null;
+public class TsukiNoTamiEntity extends PathAwareEntity {
+    private PlayerEntity attacker;
 
     protected TsukiNoTamiEntity(EntityType<? extends TsukiNoTamiEntity> entityType, World world) {
         super(entityType, world);
@@ -26,8 +20,19 @@ public class TsukiNoTamiEntity extends PathAwareEntity implements Angerable {
     @Override
     public void tick() {
         super.tick();
-        if(this.hasAngerTime()) {
-            angerTime--;
+        if(this.getTarget() != null) {
+            if(this.getTarget() instanceof PlayerEntity playerTarget) {
+                if(playerTarget.isCreative()) this.setTarget(null);
+            }
+            tryAttack(this.getTarget());
+        } else {
+            if(this.attacker != null) {
+                if(this.attacker.isCreative()) {
+                    this.attacker = null;
+                } else {
+                    this.setTarget(attacker);
+                }
+            }
         }
     }
 
@@ -42,13 +47,13 @@ public class TsukiNoTamiEntity extends PathAwareEntity implements Angerable {
             Entity attackerEntity = source.getAttacker();
             if(attackerEntity != null) {
                 if(attackerEntity instanceof PlayerEntity attackingPlayer) {
-                    this.setAttacking(attackingPlayer);
-                    this.setAttacker(attackingPlayer);
+                    if(!attackingPlayer.isCreative()) {
+                        this.attacker = attackingPlayer;
+                    }
                 } else if(attackerEntity instanceof LivingEntity attacker) {
                     this.setAttacker(attacker);
                 }
             }
-            chooseRandomAngerTime();
             return true;
         }
         return false;
@@ -58,40 +63,21 @@ public class TsukiNoTamiEntity extends PathAwareEntity implements Angerable {
     protected void initGoals() {
         super.initGoals();
         this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(1, new WanderAroundFarGoal(this, 1.0));
-        this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 16.0F));
+        this.goalSelector.add(1, new MeleeAttackGoal(this, 1.5, true));
+        this.goalSelector.add(2, new WanderAroundFarGoal(this, 1.0));
+        this.goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 16.0F));
 
-        this.targetSelector.add(1, new RevengeGoal(this, TsukiNoTamiEntity.class, RabbitEntity.class));
-        this.targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, 10, true, false, this::isPlayerAggressor));
-        this.targetSelector.add(3, new UniversalAngerGoal<>(this, false));
+        this.targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, 2, false, false, this::isPlayerAttacker));
     }
 
-    private boolean isPlayerAggressor(LivingEntity entity) {
+    private boolean isPlayerAttacker(LivingEntity entity) {
+        return entity == attacker;
+    }
+
+    @Override
+    public boolean tryAttack(Entity target) {
+        float damage = (float) this.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
+        target.damage(this.getDamageSources().mobAttack(this), damage);
         return true;
-    }
-
-    @Override
-    public int getAngerTime() {
-        return angerTime;
-    }
-
-    @Override
-    public void setAngerTime(int angerTime) {
-        this.angerTime = angerTime;
-    }
-
-    @Override
-    public @Nullable UUID getAngryAt() {
-        return targetUuid;
-    }
-
-    @Override
-    public void setAngryAt(@Nullable UUID angryAt) {
-        this.targetUuid = angryAt;
-    }
-
-    @Override
-    public void chooseRandomAngerTime() {
-        this.angerTime = this.random.nextInt(30) + 150;
     }
 }
